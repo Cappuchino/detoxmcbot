5f19ff77c9dccf43d91acbe112e61a62bff18b21
10morty <33342755+10morty@users.noreply.github.com>
2019-01-30 17:31:22 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,19 @@
-import discord
-import asyncio
+# For hosting on Heroku we'll need to use the OS library to pull the Token 
+# from the Enviroment Variables
 
-client = discord.Client()
+import os, discord
+from discord.ext.commands import Bot
+
+# We'll need to substitute the Prefix for an Enviroment Variable
+BOT_PREFIX = os.environ['!'] # -Prfix is need to declare a Command in discord ex: !pizza "!" being the Prefix
+TOKEN = os.environ['NTMzNjkwNTM2MzQ3ODI4MjI0.DzNijw.-FbIvhDAmzqyGO5EtqW1PSS4qPI'] # The token is also substituted for security reasons
+
+client = Bot(command_prefix=BOT_PREFIX)
+
+# this is an event which is triggered when something happens in Discord 
+# in this case on_ready() is called when the bot logs on
+#you can checkthe Discord API Documentaion for more event Functions 
+# here: https://discordapp.com/developers
 
 @client.event
 async def on_ready():
@@ -9,19 +21,8 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+    await client.change_presence(game=discord.Game(name="DetoxMC | Type !help"))
+    
+# below this line you can put custom Functions
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('!test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
-
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
-
-client.run('NTMzNjkwNTM2MzQ3ODI4MjI0.DzNabw.X1mDxnYVNHVjKssQ0z4pGOacDGY')
+client.run(TOKEN)
